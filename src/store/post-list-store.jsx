bd44a9892c647a17ff258875aba831b1f46d5e14
Muhammad Junaid <648mjunaid@gmail.com>
2024-12-17 02:54:11 +0500

    created structure and store for posts

---
+++ src/store/post-list-store.jsx
@@ -0,0 +1,54 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Islamabad",
+    body: "Hi friends, I am going to Islamabad to have a dinner with my cousins",
+    reactions: 2,
+    userId: "user-3",
+    tags: ["Islamabad", "Dinner", "hang out"],
+  },
+  {
+    id: "2",
+    title: "Pass ho gye bhai",
+    body: "4 saal ki masti k baad bhi ho gye hain pass!",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Exams", "Graduating", "Unbelievable"],
+  },
+];
+
+const postListReducer = (currPostList, action) => {
+    switch (action.type) {
+      case "ADD_POST":
+        return [...currPostList, action.payload];
+      case "DELETE_POST":
+        return currPostList.filter(post => post.id !== action.payload);
+      default:
+        return currPostList;
+    }
+  };
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = () => {};
+  const deletePost = () => {};
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+export default PostListProvider;
